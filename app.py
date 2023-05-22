3527283a1c565c7cb96118e7e786a9f1c5966abd
kaijutrsh <solluzumo1@gmail.com>
2023-05-22 23:56:38 +0700

    asdf

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ import logging
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-@app.route("/")
-def main():
-    # logging.info(request.json)
-    # #текст ответа, предпологается, что ответ будет в виде 'Расписание номер_группы дата'
-    # request_text = event['request']['original_utterance'].lower().split()[1::]
-    # text = db.fetchall("lesson", f"{request_text[0]};{request_text[1]}",["audience","teacher","start","end"])
-    # response = {
-    #     'version': event['version'],
-    #     'session': event['session'],
-    #     "response":{
-    #         "end_session":False,
-    #         "text" : text,
-    #     }
-    # }
-    return render_template("index.html")
+@app.route("/",methods=["POST"])
+def main(event):
+    logging.info(request.json)
+    #текст ответа, предпологается, что ответ будет в виде 'Расписание номер_группы дата'
+    request_text = event['request']['original_utterance'].lower().split()[1::]
+    text = db.fetchall("lesson", f"{request_text[0]};{request_text[1]}",["audience","teacher","start","end"])
+    response = {
+        'version': event['version'],
+        'session': event['session'],
+        "response":{
+            "end_session":False,
+            "text" : text,
+        }
+    }
+    return json.dumps(response)
