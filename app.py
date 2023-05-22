fa992ea14451536aa2937c2fab28187c5439dfe5
kaijutrsh <solluzumo1@gmail.com>
2023-05-23 00:08:45 +0700

    zxc

--- app.py
+++ app.py
@@ -11,13 +11,17 @@ def main():
     logging.info(request.json)
     #текст ответа, предпологается, что ответ будет в виде 'Расписание номер_группы дата'
     request_text = request.json['request']['original_utterance'].lower().split()[1::]
-    text = db.fetchall("lesson", f"{request_text[0]};{request_text[1]}",["audience","teacher","start","end"])
+    db_response = db.fetchall("lesson", f"{request_text[0]};{request_text[1]}",["audience","teacher","start","end"])
+    text = ""
+    for s in db_response:
+        for i in s:
+            text += f"{i} "
     response = {
         'version': request.json['version'],
         'session': request.json['session'],
         "response":{
             "end_session":False,
-            "text" : str(text),
+            "text" : text,
         }
     }
     return json.dumps(response)
--- logic.py
+++ logic.py
@@ -1,2 +1,7 @@
 import dbAPI as db
-print(db.fetchall("lesson", "432-3;22.05.2023",["audience","teacher","start","end"]))
+response = db.fetchall("lesson", "432-3;22.05.2023",["audience","teacher","start","end"])
+request=""
+for s in response:
+    for i in s:
+        request+=f"{i} "
+print(request)
