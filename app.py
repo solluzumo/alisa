ff5c0e7e951cfcef07aff7fb63d7bc776b050439
kaijutrsh <solluzumo1@gmail.com>
2023-05-22 23:59:29 +0700

    dfgjkl

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/",methods=["POST"])
-def main(event):
+def main():
     logging.info(request.json)
     #текст ответа, предпологается, что ответ будет в виде 'Расписание номер_группы дата'
-    request_text = event['request']['original_utterance'].lower().split()[1::]
+    request_text = request.json['request']['original_utterance'].lower().split()[1::]
     text = db.fetchall("lesson", f"{request_text[0]};{request_text[1]}",["audience","teacher","start","end"])
     response = {
-        'version': event['version'],
-        'session': event['session'],
+        'version': request.json['version'],
+        'session': request.json['session'],
         "response":{
             "end_session":False,
             "text" : text,
