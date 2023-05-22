a2dd31f9fa16e025ecd0612a478f1d256e94353f
kaijutrsh <solluzumo1@gmail.com>
2023-05-22 23:34:40 +0700

    test

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import dbAPI as db
-from flask import Flask, request
+from flask import Flask, request, render_template
 import json
 import logging
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-@app.route("/", methods=["POST"])
-def main(event, context):
-    logging.info(request.json)
-    #текст ответа, предпологается, что ответ будет в виде 'Расписание номер_группы дата'
-    request_text = event['request']['original_utterance'].lower().split()[1::]
-    text = db.fetchall("lesson", f"{request_text[0]};{request_text[1]}",["audience","teacher","start","end"])
-    response = {
-        'version': event['version'],
-        'session': event['session'],
-        "response":{
-            "end_session":False,
-            "text" : text,
-        }
-    }
-    return json.dumps(response)
+@app.route("/")
+def main():
+    # logging.info(request.json)
+    # #текст ответа, предпологается, что ответ будет в виде 'Расписание номер_группы дата'
+    # request_text = event['request']['original_utterance'].lower().split()[1::]
+    # text = db.fetchall("lesson", f"{request_text[0]};{request_text[1]}",["audience","teacher","start","end"])
+    # response = {
+    #     'version': event['version'],
+    #     'session': event['session'],
+    #     "response":{
+    #         "end_session":False,
+    #         "text" : text,
+    #     }
+    # }
+    return render_template("index.html")
