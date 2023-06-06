94b7c582bf7749d7d29572fc69db65f9439e66df
solluzumo <44528698+solluzumo@users.noreply.github.com>
2023-06-06 16:41:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -91,9 +91,9 @@ def main():
         try:  #пытаемся достать из базы данных расписание
             splited_response = response_gpt.split()
 
-            db_response = [list(el) for el in db.fetchall("lesson", f"{splited_response[0]};{splited_response[1]+splited_response[2]}",
+            db_response = [list(el) for el in db.fetchall("lesson", f"{splited_response[0]};{splited_response[1]} {splited_response[2]}",
                                                           ["group_name","name", "audience", "start", "end"])]
-            print(splited_response)
+            print(splited_response[1]+splited_response[2])
             print(db_response)
             text = ""
             for el in range(len(db_response)):
